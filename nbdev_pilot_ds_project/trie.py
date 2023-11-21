5f06aa0fd1307359883a97504d1eff8b5a9869d6
SergioPereo <sergio.pereo@outlook.com>
2023-11-21 15:45:53 -0600

    modify

--- nbdev_pilot_ds_project/trie.py
+++ nbdev_pilot_ds_project/trie.py
@@ -5,7 +5,7 @@ __all__ = ['TrieNode', 'Trie']
 
 # %% ../nbs/01_trie.ipynb 3
 class TrieNode:
-    "Stores the hashtable of the letters and the if that node is an end of a word."
+    "Stores the hashtable of the letters and tells if that node is the end of a word."
     def __init__(self) -> None:
         self.end = False
         self.letters = {}
@@ -15,30 +15,14 @@ class Trie:
     "Trie data structure. Can store multiple words in a graph linking each consecutive letters and make branches in them."
     def __init__(self) -> None:
         self.head = TrieNode()
-
-# %% ../nbs/01_trie.ipynb 6
-@patch
-def insert(self: Trie, 
+    def insert(self, 
             word: str # word to store in the trie
             ) -> bool: 
-    actual_iteration = self.head
-    for letter in word:
-        actual_iteration_letters = actual_iteration.letters
-        if not letter in actual_iteration_letters:
-            actual_iteration_letters[letter] = TrieNode()
-        actual_iteration=actual_iteration_letters[letter]
-    actual_iteration.end = True
-    return True
-
-# %% ../nbs/01_trie.ipynb 8
-@patch
-def search(self: Trie, 
-            word: str # check if the word is stored in the trie
-            ) -> bool: 
-    actual_iteration = self.head
-    for letter in word:
-        actual_iteration_letters = actual_iteration.letters
-        if not letter in actual_iteration_letters:
-            return False
-        actual_iteration=actual_iteration_letters[letter]
-    return actual_iteration.end
+        actual_iteration = self.head
+        for letter in word:
+            actual_iteration_letters = actual_iteration.letters
+            if not letter in actual_iteration_letters:
+                actual_iteration_letters[letter] = TrieNode()
+            actual_iteration=actual_iteration_letters[letter]
+        actual_iteration.end = True
+        return True
